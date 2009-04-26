3df5a6dc75b0b39950ac3fd7e800f2097d51a747
Justin Love <hg@JustinLove.name>
2009-04-26 11:38:27 -0500

    separate test cases

---
+++ script/testcases.js
@@ -0,0 +1,29 @@
+// Creative Commons Attribution-Share Alike 3.0 Unported Licence
+// http://creativecommons.org/licenses/by-sa/3.0/
+
+CGD.TEST.pjs.cases = {
+  composite: function(context) {
+    context.save();
+    context.rotate(CGD.JS.RADIANS*0.06);
+    context.beginPath();
+    context.moveTo(0, 0);
+    context.arc(0, 0, 0.4, 0, CGD.JS.RADIANS*0.75, CGD.ARC.CLOCKWISE);
+    context.closePath();
+    context.fillStyle = 'blue';
+    context.fill();
+    context.lineWidth = 0.1;
+    context.strokeStyle = 'green';
+    context.stroke();
+    context.save();
+    context.translate(0, 0.2);
+    context.scale(0.5, 0.5);
+    context.drawImage(document.getElementById('tiny'), -0.5, -0.5, 1, 1);
+    context.restore();
+    context.save();
+    context.translate(0.2, -0.2);
+    context.scale(0.25, 0.25);
+    context.drawImage(document.getElementById('tiny'), 1, 1, 1, 1, -0.5, -0.5, 1, 1);
+    context.restore();
+    context.restore();
+  }
+};
--- script/testpjs.js
+++ script/testpjs.js
@@ -1,6 +1,8 @@
 // Creative Commons Attribution-Share Alike 3.0 Unported Licence
 // http://creativecommons.org/licenses/by-sa/3.0/
 
+CGD.JS.require('script/testcases.js');
+
 CGD.TEST = CGD.TEST || {};
 CGD.TEST.pjs = function () {
   var t = arguments.callee;
@@ -14,38 +16,13 @@ CGD.TEST.pjs = function () {
     t.manual = new CGD.PJS.CanvasRenderingContextPostscript(t.target);
     t.get = t.target.getContext('cgd-postscript');
   }
-  CGD.DRAW.on('target', t.draw, {origin: 'center'});
+  CGD.DRAW.on('target', t.cases.composite, {origin: 'center'});
   //CGD.DRAW.saveFile = CGD.DRAW.saveFile || CGD.STRING.serial("~/Desktop/screen000.png");
   //CGD.DRAW.saveFile = "~/Desktop/pjs.png";
-  CGD.DRAW.save('target');
-  var ps = CGD.PJS.on('target', t.draw, {origin: 'center'});
+  //CGD.DRAW.save('target');
+  var ps = CGD.PJS.on('target', t.cases.composite, {origin: 'center'});
   document.getElementById('output').innerHTML = ps;
   //CGD.FILE.saveString(ps, "~/Desktop/pjs.eps");
-};
-
-CGD.TEST.pjs.draw = function(context) {
-  context.save();
-  context.rotate(CGD.JS.RADIANS*0.06);
-  context.beginPath();
-  context.moveTo(0, 0);
-  context.arc(0, 0, 0.4, 0, CGD.JS.RADIANS*0.75, CGD.ARC.CLOCKWISE);
-  context.closePath();
-  context.fillStyle = 'blue';
-  context.fill();
-  context.lineWidth = 0.1;
-  context.strokeStyle = 'green';
-  context.stroke();
-  context.save();
-  context.translate(0, 0.2);
-  context.scale(0.5, 0.5);
-  context.drawImage(document.getElementById('tiny'), -0.5, -0.5, 1, 1);
-  context.restore();
-  context.save();
-  context.translate(0.2, -0.2);
-  context.scale(0.25, 0.25);
-  context.drawImage(document.getElementById('tiny'), 1, 1, 1, 1, -0.5, -0.5, 1, 1);
-  context.restore();
-  context.restore();
 };
 
 CGD.TEST.pjs.init = function() {
